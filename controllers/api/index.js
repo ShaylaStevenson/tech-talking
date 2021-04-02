c2130918f2c52430c6300af7dbb8860d8ced9e82
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-04-01 17:22:38 -0700

    fixed comments again, testing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const blogRoutes = require('./blogRoutes');
+const blogRoutes = require('./blogRoutes');
 // const commentRoutes = require('./commentRoutes');
 
 // router.use('/comments', commentRoutes);
